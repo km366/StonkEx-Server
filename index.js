1d36d3b1aa00269d05caefd35d7337271e8c8dcf
kat.366 <km3723@drexel.edu>
2020-09-05 14:08:28 +0530

    CORS fix

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ const db = admin.firestore();
 //Hosting information
 const PORT = process.env.PORT || 5000
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://stonkex-37533.web.app'); //Change to http://localhost:PORT when using in development mode
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.json())
